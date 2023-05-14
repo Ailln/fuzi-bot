a6e5a902e2a8506ed8a945221986a037e8fbf3da
Ailln <kinggreenhall@gmail.com>
2023-05-15 01:49:17 +0800

    feat(card): add left session card and right knowlege card(in dev).

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,42 @@
 import './index.less';
 
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {
-  SettingOutlined,
+  ClearOutlined,
+  DownloadOutlined,
+  EditOutlined,
   GithubOutlined,
-  UserOutlined,
+  PlusOutlined,
   RobotOutlined,
   SendOutlined,
-  DeleteOutlined,
+  SettingOutlined,
+  UserOutlined,
 } from '@ant-design/icons';
 import {
-  Row,
-  Col,
-  Card,
-  Button,
   Avatar,
+  Button,
+  Card,
+  Col,
+  Form,
   Input,
+  List,
   message,
   Modal,
-  Form,
-  Tag,
-  Select,
   Popconfirm,
+  Row,
+  Tag,
 } from 'antd';
 import { io } from 'socket.io-client';
+import moment from 'moment';
 
 const { Meta } = Card;
 const { TextArea } = Input;
 
 export default function IndexPage() {
-  const [messages, setMessages] = useState<any | []>(
-    sessionStorage.getItem('MESSAGES') || [],
-  );
-  const [modelType, setModelType] = useState(
-    sessionStorage.getItem('MODEL_TYPE') || 'JointNLU',
-  );
   const [inputValue, setInputValue] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
   const [socketUrl, setSocketUrl] = useState(
-    sessionStorage.getItem('SOCKET_URL') || window.location.origin,
+    localStorage.getItem('SOCKET_URL') || window.location.origin,
   );
   const [socket, setSocket] = useState(
     io(socketUrl, {
@@ -46,6 +44,29 @@ export default function IndexPage() {
     }),
   );
   const [socketStatus, setSocketStatus] = useState('disconnected');
+  const [sessionList, setSessionList] = useState<Array<any>>([
+    {
+      name: 'new#' + moment().format('YYYYMMDD#HHmmss#SSS'),
+      messages: [],
+    },
+  ]);
+  const [sessionIndex, setSessionIndex] = useState(0);
+  const [isUpdateSessionList, setIsUpdateSessionList] = useState(true);
+  const [currentSession, setCurrentSession] = useState<any>({
+    name: 'new#' + moment().format('YYYYMMDD#HHmmss#SSS'),
+    messages: [],
+  });
+
+  useEffect(() => {
+    console.log('# init from local storage');
+    const sessionListFromLocalStorage = localStorage.getItem('SESSION_LIST');
+    if (sessionListFromLocalStorage !== null) {
+      const currentSessionList = JSON.parse(sessionListFromLocalStorage);
+      setSessionList(currentSessionList);
+      setIsUpdateSessionList(true);
+      setCurrentSession(currentSessionList[0]);
+    }
+  }, []);
 
   useEffect(() => {
     socket.on('connect', () => {
@@ -70,9 +91,15 @@ export default function IndexPage() {
       console.log('socket disconnect: ' + reason);
     });
 
-    socket.on('chat-reply', (msg) => {
-      console.log('socket receive chat-reply: ' + msg);
-      setMessages((messages: any) => [...messages, msg]);
+    socket.on('chat-reply', (message) => {
+      console.log('socket receive chat-reply: ' + message);
+      setIsUpdateSessionList(true);
+      setCurrentSession((session: any) => {
+        return {
+          ...session,
+          messages: [...session.messages, message],
+        };
+      });
     });
 
     return () => {
@@ -81,30 +108,71 @@ export default function IndexPage() {
     };
   }, [socket]);
 
-  // save session messages
+  // save session list on current session change
   useEffect(() => {
-    if (messages.length === 0) {
-      const contents = JSON.parse(sessionStorage.getItem('MESSAGES') ?? '[]');
-      if (contents.length > 0) {
-        setMessages(contents);
-      }
-    } else {
-      console.log('save messages to session storage');
-      sessionStorage.setItem('MESSAGES', JSON.stringify(messages));
+    console.log('[save] currentSession');
+    // 如果只更新 sessionIndex，则不需要更新 sessionList
+    if (isUpdateSessionList) {
+      setSessionList((sessionList) => {
+        const currentSessionList = [...sessionList];
+        currentSessionList[sessionIndex] = { ...currentSession };
+        return currentSessionList;
+      });
     }
-  }, [messages]);
+  }, [currentSession]);
+
+  // save session list
+  useEffect(() => {
+    console.log('[save] sessionList');
+    localStorage.setItem('SESSION_LIST', JSON.stringify(sessionList));
+  }, [sessionList]);
 
   // scroll to bottom
   useEffect(() => {
-    const messageContent =
-      document.getElementsByClassName('message-content')[0];
+    console.log('[scroll] to bottom');
+    const messageContent = document.getElementsByClassName('card-content')[1];
     if (messageContent) {
       messageContent.scrollTo({
         top: messageContent.scrollHeight,
         behavior: 'smooth',
       });
     }
-  }, [messages.length]);
+  }, [currentSession.messages.length]);
+
+  const addNewSession = () => {
+    const initSession = {
+      name: 'new#' + moment().format('YYYYMMDD#HHmmss#SSS'),
+      messages: [],
+    };
+    setSessionList((sessionList) => [{ ...initSession }, ...sessionList]);
+    setSessionIndex(0);
+    setIsUpdateSessionList(true);
+    setCurrentSession({ ...initSession });
+  };
+
+  const deleteCurrentSession = (index: any) => {
+    console.log('[delete] sessionIndex: ' + index);
+    const currentSessionList = [...sessionList];
+    currentSessionList.splice(index, 1);
+    setSessionList(currentSessionList);
+  };
+
+  const clearSessionList = () => {
+    console.log('[clear] session list');
+    const initSession = {
+      name: 'new#' + moment().format('YYYYMMDD#HHmmss#SSS'),
+      messages: [],
+    };
+    setSessionList([{ ...initSession }]);
+  };
+
+  const changeSessionIndex = (index: any) => {
+    console.log('[change] sessionIndex: ' + index);
+    setSessionIndex(index);
+
+    setIsUpdateSessionList(false);
+    setCurrentSession(sessionList[index]);
+  };
 
   const [form] = Form.useForm();
 
@@ -112,7 +180,7 @@ export default function IndexPage() {
     const formSocketUrl = form.getFieldValue('socketUrl');
     setSocketUrl(formSocketUrl);
     setSocket(io(formSocketUrl, { reconnection: false }));
-    sessionStorage.setItem('SOCKET_URL', formSocketUrl);
+    localStorage.setItem('SOCKET_URL', formSocketUrl);
     setModalVisible(false);
   };
 
@@ -122,13 +190,24 @@ export default function IndexPage() {
     // send message to api
     if (socketStatus === 'connected') {
       if (value.trim().length > 0) {
-        const message = { role: 'user', content: value };
-        setMessages((messages: any) => [...messages, message]);
-        const sendData = {
-          modelType: modelType,
-          messages: [...messages, message],
+        const updatedSession = {
+          ...currentSession,
+          messages: [
+            ...currentSession.messages,
+            { role: 'user', content: value },
+          ],
         };
-        socket.emit('chat-request', sendData);
+
+        setIsUpdateSessionList(true);
+        setCurrentSession((session: any) => {
+          return {
+            ...session,
+            messages: [...session.messages, { role: 'user', content: value }],
+          };
+        });
+
+        socket.emit('chat-request', updatedSession);
+
         setInputValue('');
       } else {
         message.error('please input message');
@@ -138,21 +217,89 @@ export default function IndexPage() {
     }
   };
 
-  const handleClear = () => {
-    console.log('clear!');
-    sessionStorage.removeItem('MESSAGES');
-    setMessages([]);
-  };
-
-  const handleModelTypeChange = (value: string) => {
-    setModelType(value);
-    sessionStorage.setItem('MODEL_TYPE', value);
-  };
-
   return (
     <div>
-      <Row className="card-body">
-        <Col xs={0} md={4} lg={6}></Col>
+      <Row className="page-body">
+        {/*左边卡片*/}
+        <Col xs={0} md={4} lg={6}>
+          <Card
+            hoverable
+            className="left-card"
+            title="Session"
+            extra={
+              <Button
+                shape="circle"
+                icon={<PlusOutlined />}
+                onClick={addNewSession}
+              />
+            }
+            actions={[
+              <Popconfirm
+                title="Confirm to DELETE all session?"
+                onConfirm={clearSessionList}
+                okText="ok"
+                cancelText="cancel"
+              >
+                <Button
+                  ghost
+                  className="full-button"
+                  type="primary"
+                  icon={<ClearOutlined />}
+                  size={'large'}
+                  danger
+                >
+                  Clear
+                </Button>
+              </Popconfirm>,
+            ]}
+          >
+            <div className="card-content">
+              <List
+                bordered
+                itemLayout="horizontal"
+                dataSource={sessionList}
+                renderItem={(item, index) => (
+                  <List.Item
+                    style={{
+                      backgroundColor:
+                        sessionIndex === index ? '#e6f4ff' : 'white',
+                    }}
+                    actions={[
+                      <Button
+                        ghost
+                        type="primary"
+                        icon={<EditOutlined />}
+                        size={'small'}
+                        onClick={() => {
+                          message.warning('to do...');
+                        }}
+                      ></Button>,
+                      <Popconfirm
+                        title="Confirm to DELETE this session?"
+                        onConfirm={() => deleteCurrentSession(index)}
+                        okText="ok"
+                        cancelText="cancel"
+                      >
+                        <Button
+                          ghost
+                          type="primary"
+                          icon={<ClearOutlined />}
+                          size={'small'}
+                          danger
+                        ></Button>
+                      </Popconfirm>,
+                    ]}
+                    onClick={() => changeSessionIndex(index)}
+                  >
+                    <List.Item.Meta title={item.name} />
+                  </List.Item>
+                )}
+              />
+            </div>
+          </Card>
+        </Col>
+
+        {/*中间卡片*/}
         <Col xs={24} md={16} lg={12}>
           <Modal
             title="Settings"
@@ -205,74 +352,40 @@ export default function IndexPage() {
             }
             actions={[
               <div className="human-input">
-                <div>
-                  <TextArea
-                    autoSize={{ minRows: 1, maxRows: 6 }}
-                    size={'large'}
-                    className="human-input-message"
-                    placeholder="please input question..."
-                    value={inputValue}
-                    onChange={(event) =>
-                      setInputValue(event.currentTarget.value)
-                    }
-                  />
-                </div>
-                <div>
-                  <Select
-                    className="human-input-select"
-                    defaultValue={modelType}
-                    onChange={handleModelTypeChange}
-                    size="large"
-                    options={[
-                      { value: 'JointNLU', label: 'JointNLU' },
-                      { value: 'ChatGLM-6B', label: 'ChatGLM-6B' },
-                      { value: 'ChatGPT', label: 'ChatGPT' },
-                    ]}
-                  />
-                  <Popconfirm
-                    title="确认要「清除」所有聊天记录吗？"
-                    onConfirm={handleClear}
-                    okText="确认"
-                    cancelText="取消"
-                  >
-                    <Button
-                      ghost
-                      className="human-input-clear-button"
-                      type="primary"
-                      icon={<DeleteOutlined />}
-                      size={'large'}
-                      danger
-                    >
-                      Clear
-                    </Button>
-                  </Popconfirm>
-                  <Button
-                    className="human-input-message-button"
-                    type="primary"
-                    size={'large'}
-                    icon={<SendOutlined />}
-                    onClick={handleSend}
-                  >
-                    Send
-                  </Button>
-                </div>
+                <TextArea
+                  autoSize={{ minRows: 1, maxRows: 6 }}
+                  size={'large'}
+                  className="human-input-message"
+                  placeholder="please input question..."
+                  value={inputValue}
+                  onChange={(event) => setInputValue(event.currentTarget.value)}
+                />
+                <Button
+                  className="human-input-message-button"
+                  type="primary"
+                  size={'large'}
+                  icon={<SendOutlined />}
+                  onClick={handleSend}
+                >
+                  <span id="human-input-message-button-text">Send</span>
+                </Button>
               </div>,
             ]}
           >
-            <div className="message-content">
-              {messages.map(
+            <div className="card-content">
+              {currentSession.messages.map(
                 (message: { role: string; content: string }, index: number) => (
                   <Meta
                     key={index}
                     className={
-                      message.role === 'bot'
+                      message.role === 'assistant'
                         ? 'left-message-card'
                         : 'right-message-card ant-card-rtl'
                     }
                     avatar={
                       <Avatar
                         icon={
-                          message.role === 'bot' ? (
+                          message.role === 'assistant' ? (
                             <RobotOutlined />
                           ) : (
                             <UserOutlined />
@@ -281,7 +394,7 @@ export default function IndexPage() {
                         size={'large'}
                       />
                     }
-                    title={message.role === 'bot' ? 'Bot' : 'User'}
+                    title={message.role === 'assistant' ? 'Assistant' : 'User'}
                     description={message.content}
                   />
                 ),
@@ -293,7 +406,45 @@ export default function IndexPage() {
             {new Date().getFullYear()}.
           </div>
         </Col>
-        <Col xs={0} md={4} lg={6}></Col>
+
+        {/*右边卡片*/}
+        <Col xs={0} md={4} lg={6}>
+          <Card
+            hoverable
+            className="right-card"
+            title={'Knowledge'}
+            extra={
+              <Button
+                shape="circle"
+                icon={<DownloadOutlined />}
+                onClick={() => {
+                  message.warning('to do...');
+                }}
+              />
+            }
+            actions={[
+              <Button
+                ghost
+                className="full-button"
+                type="primary"
+                icon={<EditOutlined />}
+                size={'large'}
+                onClick={() => {
+                  message.warning('to do...');
+                }}
+              >
+                Edit
+              </Button>,
+            ]}
+          >
+            <div className="card-content">
+              <TextArea
+                placeholder="please input prompt..."
+                style={{ height: '100%' }}
+              />
+            </div>
+          </Card>
+        </Col>
       </Row>
     </div>
   );
